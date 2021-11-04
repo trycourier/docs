8a2d874609d6ff0bb7a7e9c3f078dbef22d393a3
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-11-05 03:30:18 +0400

    Apply latest changes from OpenAPI

--- docs/reference/configs/schemas/MessageHistory.ts
+++ docs/reference/configs/schemas/MessageHistory.ts
@@ -1,22 +1,24 @@
 import { ApiParam } from "@site/src/components/ApiReference/ApiParamField";
 
-import MessageHistoryType from "./MessageHistoryType";
+import MessageHistoryEnqueuedItem from "./MessageHistoryEnqueuedItem";
+import MessageHistoryMappedItem from "./MessageHistoryMappedItem";
+import MessageHistoryProfileLoadedItem from "./MessageHistoryProfileLoadedItem";
+import MessageHistoryRenderedItem from "./MessageHistoryRenderedItem";
+import MessageHistoryRoutedItem from "./MessageHistoryRoutedItem";
+import MessageHistoryDeliveredItem from "./MessageHistoryDeliveredItem";
+import MessageHistoryUndeliverableItem from "./MessageHistoryUndeliverableItem";
 
 const MessageHistory: ApiParam = {
-  type: "object",
+  type: "oneOf",
   displayName: "MessageHistory",
-  fields: [
-    {
-      type: "number",
-      name: "ts",
-      example: 1562611083411,
-      description:
-        "A UTC timestamp at which the recipient opened a message for the first time. This is stored as a millisecond representation of the Unix epoch (the time passed since January 1, 1970).",
-    },
-    {
-      ...MessageHistoryType,
-      name: "type",
-    },
+  options: [
+    MessageHistoryEnqueuedItem,
+    MessageHistoryMappedItem,
+    MessageHistoryProfileLoadedItem,
+    MessageHistoryRenderedItem,
+    MessageHistoryRoutedItem,
+    MessageHistoryDeliveredItem,
+    MessageHistoryUndeliverableItem,
   ],
 };
 
---
+++ docs/reference/configs/schemas/MessageHistoryProfileLoadedItem.ts
@@ -0,0 +1,41 @@
+import { ApiParam } from "@site/src/components/ApiReference/ApiParamField";
+
+const MessageHistoryProfileLoadedItem: ApiParam = {
+  type: "object",
+  displayName: "MessageHistoryProfileLoadedItem",
+  fields: [
+    {
+      type: "json",
+      name: "merged_profile",
+      description: "The profile merged while sending the message",
+      example: {},
+    },
+    {
+      type: "json",
+      name: "received_profile",
+      description: "The profile received while sending the message",
+      example: {},
+    },
+    {
+      type: "json",
+      name: "stored_profile",
+      description: "The profile stored while sending the message",
+      example: {},
+    },
+    {
+      type: "number",
+      name: "ts",
+      example: 1562611083411,
+      description:
+        "A UTC timestamp at which the recipient opened a message for the first time. This is stored as a millisecond representation of the Unix epoch (the time passed since January 1, 1970).",
+    },
+    {
+      type: "string",
+      name: "type",
+      enum: ["PROFILE_LOADED"],
+      example: "PROFILE_LOADED",
+    },
+  ],
+};
+
+export default MessageHistoryProfileLoadedItem;
