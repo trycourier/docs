74456dc6f0b35fc374987a250e9b04e1389e37f2
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-11-09 16:39:53 +0400

    Create Youtube Embed component

---
+++ src/components/YoutubeEmbed/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+interface YoutubeEmbedProps {
+  videoId: string;
+}
+
+const YoutubeEmbed = ({ videoId }: YoutubeEmbedProps) => (
+  <div className={styles.youtubeEmbed}>
+    <iframe
+      src={`https://www.youtube.com/embed/${videoId}`}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </div>
+);
+
+export default YoutubeEmbed;
