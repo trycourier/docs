e95b880ab5cdd57491046ef2b9ec45d6d6dec624
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-01-23 14:33:02 -0800

    /send spec updates.

--- versioned_docs/version-2.0.0/reference/configs/schemas/Send.ts
+++ versioned_docs/version-2.0.0/reference/configs/schemas/Send.ts
@@ -1,57 +1,266 @@
 import { ApiParam } from "@site/src/components/ApiReference/ApiParamField";
 
+const to: ApiParam = {
+  type: "object",
+  name: "to",
+  required: true,
+  description: 'The recipient of the message.',
+  fields: [
+    {
+      type: "object",
+      name: "data",
+      fields: [],
+      description: "An object that includes any data you want to pass to the message. The data will populate the corresponding template or elemental variables."
+    },
+    {
+      type: "string",
+      name: "email",
+      description: "A unique email address associated to the recipient of message."
+    },
+    {
+      type: "string",
+      name: "user_id",
+      description: "A unique identifier associated with the recipient of the delivered message."
+    },
+    {
+      type: "string",
+      name: "phone_number",
+      description: "A unique phone number associated to the recipient of message."
+    },
+    {
+      type: "object",
+      name: "preferences",
+      description: "An object that includes any preferences for the recipient.",
+      fields: []
+    },
+  ]
+};
+
+const brand_id: ApiParam = {
+  type: "string",
+  name: "brand_id",
+  description: "A unique identifier that represents the brand that should be used for rendering the notification. Note that a brand_id will only be applied to a Template message."
+}
+
+const channelsEmail: ApiParam = {
+  type: "object",
+  name: "email",
+  fields: [
+    {
+      type: "string",
+      name: "brand_id",
+    },
+    {
+      type: "array",
+      name: "providers",
+      field: {
+        type: "string",
+      },
+    },
+    {
+      type: "string",
+      name: "routing_method",
+      enum: ["all", "single"]
+    },
+    {
+      type: "string",
+      name: "if"
+    },
+    {
+      type:"object",
+      name: "override",
+      fields: []
+    }
+  ]
+}
+
+const channelsPush: ApiParam = {
+  type: "object",
+  name: "push",
+  fields: [
+    {
+      type: "string",
+      name: "brand_id",
+    },
+    {
+      type: "array",
+      name: "providers",
+      field: {
+        type: "string",
+      },
+    },
+    {
+      type: "string",
+      name: "routing_method",
+      enum: ["all", "single"]
+    },
+    {
+      type: "string",
+      name: "if"
+    },
+    {
+      type:"object",
+      name: "override",
+      fields: []
+    }
+  ]
+}
+
+const channels: ApiParam = {
+  type: "object",
+  name: "channels",
+  description: "A map of valid channel identifiers (e.g. email, sms, etc.) to channel configuration objects whose properties are below. If you don't specify 'channels', Courier will use the default configuration for each channel as specified in the Courier Studio UI.",
+  fields: [
+    channelsPush,
+    channelsEmail
+  ]
+}
+
+const routingStrategyProvider: ApiParam = {
+  type: "object",
+  fields: [
+    {
+      type: "string",
+      name: "name"
+    },
+    {
+      type: "object",
+      name: "config",
+      fields: []
+    },
+    {
+      type: "string",
+      name: "if"
+    }
+  ]
+}
+
+const routingStrategyChannel: ApiParam = {
+  type: "object",
+  fields: [
+    {
+      type: "string",
+      name: "channel"
+    },
+    {
+      type: "object",
+      name: "config",
+      fields: []
+    },
+    {
+      type: "array",
+      name: "providers",
+      field: {
+        type: "oneOf",
+        options: [
+          routingStrategyProvider,
+          {
+            type: "string"
+          }
+        ]
+      }
+    },
+    {
+      type: "string",
+      name: "if"
+    }
+  ]
+}
+
+const routingChannel: ApiParam = {
+  type: "oneOf",
+  options: [
+    routingStrategyChannel,
+    routingStrategyProvider,
+    {
+      type: "string"
+    }
+  ]
+}
+
+const routing: ApiParam = {
+  type:"object",
+  name: "routing",
+  description: "Allows you to customize which channel(s) Courier will potentially deliver the message. If no routing key is specified, Courier will use the default routing configured in the Courier Studio UI.",
+  fields: [
+    {
+      type: "string",
+      name: "method",
+      required: true,
+      enum: ["all", "single"]
+    },
+    {
+      type: "array",
+      name: "channels",
+      field: {
+        type: "object",
+        fields: [
+          {
+            type: "string",
+            name: "method",
+            required: true,
+            enum: ["all", "single"]
+          },
+          {
+            type: "array",
+            name: "channels",
+            field: {
+              type: "object",
+              fields: []
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+
+const providers: ApiParam = {
+  type: "object",
+  name: "providers",
+  description: "The providers object is a map of valid provider identifiers (e.g. twilio, slack, etc.) to channel configuration objects. If you don't specify providers, Courier will use the default configuration for each provider as specified in the Courier Studio UI.",
+  fields: [
+    {
+      type: "object",
+      fields: [
+        {
+          type: "object",
+          name: "override",
+          fields: []
+        },
+        {
+          type: "string",
+          name: "if"
+        }
+      ]
+    }
+  ]
+}
+
 const Send: ApiParam = {
   type: "object",
   displayName: "Send",
   fields: [
+    to,
+    brand_id,
+    routing,
+    channels,
+    providers,
     {
       type: "string",
-      name: "event",
-      required: true,
-      example: "EXAMPLE_NOTIFICATION",
-      description:
-        'A unique identifier that can be mapped to an individual Notification. This could be the "Notification ID” on Notification detail pages (see the [Notifications page](https://app.courier.com/designer/notifications) in the Courier app) or a custom string mapped to the event in [settings](https://app.courier.com/settings/events).',
+      name: "template"
     },
     {
-      type: "string",
-      name: "recipient",
-      required: true,
-      example: "8ec8c99a-c5f7-455b-9f60-8222b8a27056",
-      description: "A unique identifier associated with the recipient of the delivered message.",
+      type: "object",
+      name: "content",
+      fields: []
     },
     {
-      type: "string",
-      name: "brand",
-      example: "W50NC77P524K14M5300PGPEK4JMJ",
-      description:
-        "A unique identifier that represents the brand that should be used for rendering the notification.",
-    },
-    {
-      type: "json",
-      name: "data",
-      example: { name: "Jane Doe", age: 27 },
-      description:
-        "An object that includes any data you want to pass to a message template. The data will populate the corresponding template variables.",
-    },
-    {
-      type: "json",
-      name: "override",
-      description:
-        "An object that is merged into the request sent by Courier to the provider to override properties or to gain access to features in the provider API that are not natively supported by Courier.",
-    },
-    {
-      type: "json",
-      name: "preferences",
-      description: "An object that includes any preferences for the recipient.",
-    },
-    {
-      type: "json",
-      name: "profile",
-      example: { phone_number: "2025550125", email: "hello@example.com" },
-      description:
-        "An object that includes any key-value pairs required by your chosen Integrations (see our [Provider Documentation](/docs/guides/providers) for the requirements for each Integration.) If profile information is included in the request and that information already exists in the profile for the recipientId, that information will be merged.",
-    },
-  ],
+      type: "object",
+      name: "metadata",
+      fields: []
+    }
+  ]
 };
 
 export default Send;
