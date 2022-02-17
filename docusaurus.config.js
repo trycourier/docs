fd260dd2394c38c62417ca31664904967565ad93
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-02-17 12:34:16 +0400

    Update version banners

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const metadata = require("./src/theme/metadata");
               current: {
                 label: "1.0.0",
                 path: "1.0.0",
-                banner: "unmaintained",
+                banner: "none",
                 badge: false,
               },
               "2.0.0": {
