e229fbda70c683789a655ea8e5f069d4cdcd9969
Nikhil Ismail <nikhil.ismail@Nikhils-MacBook-Pro.local>
2022-07-21 15:12:56 -0700

    [c-6366] audit events api courier docs reference (get all events)

---
+++ docs/reference/configs/schemas/AuditEvent.ts
@@ -0,0 +1,76 @@
+import { ApiParam } from "@site/src/components/ApiReference/ApiParamField";
+
+const AuditEvent: ApiParam = {
+  type: "object",
+  displayName: "AuditEvent",
+  fields: [
+    {
+        type: "string",
+        name: "auditEventId",
+        description: "A unique identifier associated with the audit event you wish to retrieve from the Audit Trail.",
+        example: "1-5e2b2615-05efbb3acab9172f88dd3f6f"
+    },
+    {
+        type: "object",
+        name: "actor",
+        fields: [
+            {
+                type: "string",
+                name: "id",
+                description: "A unique identifier associated with the actor of the audit event.",
+                example: "CC607F6E84A34305AE98B72C"
+            },
+            {
+                type: "string",
+                name: "email",
+                description: "The email asscoiated with the actor of the audit event.",
+                example: "joe.fake@gmail.com"
+            },
+        ],
+    },
+    {
+        type: "object",
+        name: "target",
+        fields: [
+            {
+                type: "string",
+                name: "id",
+                description: "A unique identifier associated with the target of the audit event.",
+                example: "EF607F6E84A34305AE98B6TR",
+            },
+            {
+                type: "string",
+                name: "email",
+                description: "The email asscoiated with the target of the audit event.",
+                example: "jane.doe@gmail.com",
+            },
+        ],
+    },
+    {
+        type: "string",
+        name: "source",
+        description: "A unique identifier representing the source of the audit event.",
+        example: "courier.studio"
+    },
+    {
+        type: "string",
+        name: "timestamp",
+        description: "A UTC timestamp at which the audit event was triggered.",
+        example: "courier.studio"
+    },
+    {
+        type: "string",
+        name: "type",
+        description: "The type of audit event that was triggered",
+        example: "user:role-changed"
+    },
+    {
+        type: "string",
+        name: "workspaceId",
+        description: "A unique identifier associated with the Audit Trail's workspace.",
+        example: "876a5b62-1d7c-494f-9a32-d40a39a8e29e"
+    },
+  ],
+};
+
+export default AuditEvent;
---
+++ versioned_docs/version-2.0.0/reference/configs/audit-trail/list.ts
@@ -0,0 +1,41 @@
+import { ApiReferenceProps } from "@site/src/components/ApiReference";
+
+import Paging from "../schemas/Paging";
+import AuditEvent from "../schemas/AuditEvent";
+
+const config: ApiReferenceProps = {
+  description: "Fetch the list of audit events",
+  method: "GET",
+  path: "/audit-events",
+  queryParams: [
+    {
+      type: "string",
+      name: "cursor",
+      description: "A unique identifier that allows for fetching the next set of audit events.",
+      example: "MTU4OTQ5NTI1ODY4NywxLTVlYmRjNWRhLTEwODZlYWFjMWRmMjEwMTNjM2I0ZjVhMA==",
+    }
+  ],
+  responses: [
+    {
+      status: 200,
+      description: "OK",
+      body: {
+        type: "object",
+        fields: [
+          {
+            ...Paging,
+            name: "paging",
+          },
+          {
+            type: "array",
+            description: "An array of Audit Events",
+            field: AuditEvent,
+            name: "results",
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default config;
