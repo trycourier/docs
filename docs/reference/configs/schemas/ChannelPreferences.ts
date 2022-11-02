a48aaa0eb8fbef38d4ab239cc344c0e6bc33855a
Suhas Deshpande <suhasdeshpande@users.noreply.github.com>
2022-11-02 11:51:37 -0700

    Update docs/reference/configs/schemas/ChannelPreferences.ts

--- docs/reference/configs/schemas/ChannelPreferences.ts
+++ docs/reference/configs/schemas/ChannelPreferences.ts
@@ -4,7 +4,7 @@ const ChannelPreferences: ApiParam = {
   type: "object",
   displayName: "ChannelPreferences",
   description:
-    "This property is used to set the preferred channel for a given user. When you send a notification to the user with `channel_preferences`, Courier will use the preferred channel if it is available. If the preferred channel is not available, Courier will use the next available channel. Courier will only send to the first available preferred channel.",
+    "This property is used for setting the preferred channel for a given user. When you send a notification to the user with `channel_preferences`, Courier will use the preferred channel if it is available. If the preferred channel is not available, Courier will use the next available channel. Courier will only send to the first available preferred channel.",
   fields: [
     {
       type: "string",
