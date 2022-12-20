b1a92023f690cd03df51d2e5397120e11f0f33b3
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2022-12-20 23:08:56 +0400

    Fix OG Meta tags overwrite

--- src/theme/metadata.js
+++ src/theme/metadata.js
@@ -1,21 +1,7 @@
 const metadata = [
   {
-    name: "description",
-    content:
-      "Courier provides one unified API to send multi-channel notifications. Notify users over email, SMS, push, and messaging apps like Slack, WhatsApp, and more.",
-  },
-  {
     name: "twitter:card",
     content: "summary",
-  },
-  {
-    name: "og:title",
-    content: "Courier Docs: API Reference, Quickstart Guides, and Integrations",
-  },
-  {
-    name: "og:description",
-    content:
-      "API reference, quickstart guides, and code snippets for integrating the Courier API into your web or mobile app. SDKs for popular programming languages.",
   },
   {
     name: "og:site_name",
