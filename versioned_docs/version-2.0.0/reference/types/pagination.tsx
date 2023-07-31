875d5d31779090c3243cb6398632ac0296351d82
Drew Youngwerth <drew@youngwerth.com>
2023-07-31 13:13:13 -0700

    Fixes

--- versioned_docs/version-2.0.0/reference/types/pagination.tsx
+++ versioned_docs/version-2.0.0/reference/types/pagination.tsx
@@ -7,11 +7,32 @@ export const PaginatedResponse: FC<{ type: string }> = ({ type, children }) => (
       {children}
     </Param>
     <Param name="has_more" type="boolean">
-      Set to <code>true</code> when there are more items that can be requested.
+      Set to true when there are more pages that can be retrieved.
+    </Param>
+    <Param name="url" type="string">
+      A url that may be used to generate these results.
+    </Param>
+    <Param name="next_url" type="string?">
+      A url that may be used to generate fetch the next set of results. Defined only when
+      <code>has_more</code> is set to true
     </Param>
     <Param name="cursor" type="string?">
-      A cursor to provide for the starting_after parameter. Will be supplied whenver{" "}
-      <code>has_more</code> is true.
+      A pointer to the next page of results. Defined only when
+      <code>has_more</code> is set to true
+    </Param>
+    <Param name="type" type="'list">
+      Always set to <code>"list"</code>. Represents the type of this object.
+    </Param>
+  </Params>
+);
+
+export const PaginatedRequestQueryParams = () => (
+  <Params>
+    <Param name="limit" type="number">
+      The number of accounts to return (defaults to 20, maximum value of 100)
+    </Param>
+    <Param name="starting_after" type="string">
+      Value of next_page from previous response
     </Param>
   </Params>
 );
