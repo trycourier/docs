909439c3d0e9982307149a10be618c49029aeba3
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-08-17 18:25:37 +0600

    update receipt-subscribe.mdx

--- src/components/ApiReferenceUpdate/ApiParamRecordField.tsx
+++ src/components/ApiReferenceUpdate/ApiParamRecordField.tsx
@@ -10,41 +10,39 @@ import Markdown from "markdown-to-jsx";
 
 const ApiParamRecordField = ({ param, field, form }: FieldComponentProps<"array">) => {
   return (
-    <Params>
-      <Param name={param.name} type={param.type}>
-        {param.description && <Markdown>{param.description}</Markdown>}
-        {Object.entries(field.value).length > 0 && (
-          <ParamsList>
-            {Object.entries(field.value).map(([key], index) => (
-              <div key={index} className={styles.paramContainer}>
-                <div className={styles.negativeButtonContainerStyle}>
-                  <button
-                    type="button"
-                    onClick={() => form.setFieldValue(field.name, omit(field.value, key))}
-                    className={styles.negativeButtonStyle}
-                  >
-                    - {param.name}[KEY]
-                  </button>
-                </div>
-                <ApiParamField param={param.field} prefix={`${field.name}[${key}]`} />
+    <Param name={param.name} type={param.type}>
+      {param.description && <Markdown>{param.description}</Markdown>}
+      {Object.entries(field.value).length > 0 && (
+        <ParamsList>
+          {Object.entries(field.value).map(([key], index) => (
+            <div key={index} className={styles.paramContainer}>
+              <div className={styles.negativeButtonContainerStyle}>
+                <button
+                  type="button"
+                  onClick={() => form.setFieldValue(field.name, omit(field.value, key))}
+                  className={styles.negativeButtonStyle}
+                >
+                  - {param.name}[KEY]
+                </button>
               </div>
-            ))}
-          </ParamsList>
-        )}
-        <ExpandButton
-          onClick={() => {
-            if (field.value[""]) return;
+              <ApiParamField param={param.field} prefix={`${field.name}[${key}]`} />
+            </div>
+          ))}
+        </ParamsList>
+      )}
+      <ExpandButton
+        onClick={() => {
+          if (field.value[""]) return;
 
-            form.setFieldValue(field.name, {
-              ...field.value,
-              "": apiParamInitialValue(param.field),
-            });
-          }}
-        >
-          + ADD
-        </ExpandButton>
-      </Param>
-    </Params>
+          form.setFieldValue(field.name, {
+            ...field.value,
+            "": apiParamInitialValue(param.field),
+          });
+        }}
+      >
+        + ADD
+      </ExpandButton>
+    </Param>
   );
 };
 
