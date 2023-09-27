adc3497052a8607a00e0ce3ae70554fdcf2dc2f7
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-09-28 01:46:02 +0600

    add api response log

--- src/components/ApiReference/ApiReferenceToken.tsx
+++ src/components/ApiReference/ApiReferenceToken.tsx
@@ -44,6 +44,7 @@ export const ApiReferenceTokenProvider = (props: React.PropsWithChildren<{}>) =>
     })
       .then((response) => {
         if (!response.ok) throw new Error();
+
         return response.json();
       })
       .then((body) => {
