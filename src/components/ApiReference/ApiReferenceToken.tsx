1565bcd152a36650b711080e08604647b15d59d2
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-09-28 01:44:29 +0600

    fix mobile nav color

--- src/components/ApiReference/ApiReferenceToken.tsx
+++ src/components/ApiReference/ApiReferenceToken.tsx
@@ -44,10 +44,10 @@ export const ApiReferenceTokenProvider = (props: React.PropsWithChildren<{}>) =>
     })
       .then((response) => {
         if (!response.ok) throw new Error();
-
         return response.json();
       })
       .then((body) => {
+        console.log("respbody", body);
         setToken(body.token);
         setSsoToken(body.token);
       })
