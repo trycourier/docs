c919201f6213ad713c147a1d13958d0e45261761
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2023-11-01 18:30:07 +0600

    implement filter

--- src/components/TagsList/TutorialsTags.tsx
+++ src/components/TagsList/TutorialsTags.tsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePluginData } from "@docusaurus/useGlobalData";
 import TagsList from "./TagsList";
 import Link from "@docusaurus/Link";
 import styles from "./tutorialsTags.module.css";
 
+type DataType = { slug: string; title: string; tags: string[] };
+
 const TutorialsTags = () => {
   const { tutorialsTags = [], allData } = usePluginData("my-plugin") as {
     tutorialsTags: string[];
-    allData: { slug: string; title: string; tags: string[] }[];
+    allData: DataType[];
   };
+
+  const [selectedTagsData, setSelectedTagsData] = useState<DataType[]>(allData);
 
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const handleTagsSelection = (tag: string) => {
@@ -21,8 +25,16 @@ const TutorialsTags = () => {
     setSelectedTags(nextTags);
   };
 
-  // todo: implement filtering
-  // console.log("allData", allData);
+  useEffect(() => {
+    if (selectedTags.length === 0) {
+      setSelectedTagsData(allData);
+      return;
+    }
+    const filteredData = allData.filter((item) =>
+      item?.tags?.some((tag) => selectedTags.includes(tag))
+    );
+    setSelectedTagsData(filteredData);
+  }, [selectedTags, allData]);
 
   return (
     <div className={styles.container}>
@@ -34,14 +46,16 @@ const TutorialsTags = () => {
         />
       </div>
       <ul>
-        {allData.map((data) => (
+        {selectedTagsData.map((data) => (
           <li key={data.slug}>
             <div className={styles.listItem}>
               <Link href={`./${data.slug}`}>{data.title}</Link>
               {data?.tags?.length > 0 && (
                 <ul className={styles.tagList}>
                   {data.tags.map((tag) => (
-                    <li className={styles.tagListItem}>{tag}</li>
+                    <li className={styles.tagListItem} key={tag}>
+                      {tag}
+                    </li>
                   ))}
                 </ul>
               )}
