9523174cbb0a2d2819e8d620edfc790a760c89f0
Jonathon Allen <945646@gmail.com>
2024-09-11 11:28:35 -0700

    search reinstall

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,4 +118,20 @@ module.exports = {
       async: true,
     },
   ],
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
 };
