f55d11ac56aa5ba2d9517ef4d7b963f105665d21
Jonathon Allen <945646@gmail.com>
2024-10-01 16:18:45 -0700

    Button component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,10 +1,12 @@
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents";
 import * as ParamsModules from "@site/src/components/Params";
+import Button from "../components/Button";
 
 const modules = {
   ...MDXComponents,
   ...ParamsModules,
+  Button,
 };
 
 export default modules;
